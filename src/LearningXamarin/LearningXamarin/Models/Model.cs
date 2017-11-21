1918f57e9aff9c8b7c3592a3004f7c40ebad326a
Keita Watanabe <Nal3eCh@gmail.com>
2017-11-21 21:32:56 +0900

    add models

---
+++ src/LearningXamarin/LearningXamarin/Models/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningXamarin.Models
+{
+    public class Model
+    {
+        public string Text { get; set; }
+
+        public Model(IHogeInterface hoge)
+        {
+            Text = hoge.Text;
+        }
+    }
+}
