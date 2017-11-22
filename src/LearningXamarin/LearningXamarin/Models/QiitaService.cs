affc03ea50e360333dc18d55330c2f33e67ede8b
Keita Watanabe <Nal3eCh@gmail.com>
2017-11-22 23:32:10 +0900

    create qiita service

---
+++ src/LearningXamarin/LearningXamarin/Models/QiitaService.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace LearningXamarin.Models
+{
+    public class QiitaService
+    {
+        private static readonly string BASE_URL = "https://qiita.com/api/v2/";
+        private static readonly HttpClient client = new HttpClient();
+
+        public IObservable<QiitaContent> FetchByQuery(string query)
+        {
+            return Observable.Create<QiitaContent>(observer =>
+            {
+                var responseString = client.GetStringAsync($"{BASE_URL}items?query={query}").Result;
+
+                var contents = JsonConvert.DeserializeObject<List<QiitaContent>>(responseString);
+
+                contents.ForEach(r => observer.OnNext(r));
+                observer.OnCompleted();
+
+                return () => { };
+            });
+        }
+    }
+}
