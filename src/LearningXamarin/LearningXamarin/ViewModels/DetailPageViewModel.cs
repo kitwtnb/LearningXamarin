4d1b1bbdf69cf12ef061511ad9b6208022c9a5d2
Keita Watanabe <Nal3eCh@gmail.com>
2017-11-26 12:36:57 +0900

    implement navigation

--- src/LearningXamarin/LearningXamarin/ViewModels/DetailPageViewModel.cs
+++ src/LearningXamarin/LearningXamarin/ViewModels/DetailPageViewModel.cs
@@ -9,6 +9,9 @@ namespace LearningXamarin.ViewModels
 {
 	public class DetailPageViewModel : ViewModelBase
 	{
+        private static readonly string ParamKeyTitle = "title";
+        private static readonly string ParamKeyUrl = "url";
+
         private string _url;
         public string URL
         {
@@ -16,10 +19,25 @@ namespace LearningXamarin.ViewModels
             set { SetProperty(ref _url, value); }
         }
 
+        public static NavigationParameters CreateParameter(string title, string url)
+        {
+            return new NavigationParameters()
+            {
+                { ParamKeyTitle, title },
+                { ParamKeyUrl, url },
+            };
+        }
+
         public DetailPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
 
+        }
+
+        public override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            Title = (string)parameters[ParamKeyTitle];
+            URL = (string) parameters[ParamKeyUrl];
         }
     }
 }
